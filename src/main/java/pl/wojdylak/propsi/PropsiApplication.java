a3290c95dcc7f59dd29764786b26a3aab7548796
Jan Wojdylak <jrwojdylak@gmail.com>
2022-07-30 23:12:15 +0200

    Initial commit

---
+++ src/main/java/pl/wojdylak/propsi/PropsiApplication.java
@@ -0,0 +1,13 @@
+package pl.wojdylak.propsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wojdylak/propsi/PropsiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojdylak.propsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
